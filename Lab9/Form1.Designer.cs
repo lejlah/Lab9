ac2c0ab135ea283df5c495757fb6a981803baa0c
lejlah <lejlasalic96@gmail.com>
2017-04-26 09:53:34 +0200

    New functionality

--- Lab9/Form1.Designer.cs
+++ Lab9/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(108, 111);
+            this.label1.Location = new System.Drawing.Point(119, 113);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 13);
             this.label1.TabIndex = 1;
